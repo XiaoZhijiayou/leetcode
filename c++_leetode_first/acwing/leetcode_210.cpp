80820f27fa5c825f350e2b3c3e4d43ae3a4aa57a
XiaoZhijiayou <lzh17737135155@163.com>
2024-10-26 18:51:17 +0800

    acwing:210

---
+++ c++_leetode_first/acwing/leetcode_210.cpp
@@ -0,0 +1,51 @@
+//
+// Created by 11818 on 2024/10/26.
+//
+#include <vector>
+#include <queue>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> grapth(numCourses);
+        vector<int> depth(numCourses,0);
+        //开始创建拓扑表
+        for(auto i : prerequisites){
+            grapth[i[1]].push_back(i[0]);
+            depth[i[0]]++;
+        }
+        //然后开始找出来没有度的课程节点，将他加入遍历的队列中去。
+        queue<int> que;
+        for(int i = 0; i < depth.size();i++){
+            if(depth[i] == 0)
+                que.push(i);
+        }
+        //然后开始遍历队列
+        vector<int> result;
+        while(que.size()){
+            auto t = que.front();
+            que.pop();
+            result.push_back(t);
+            for(auto i : grapth[t]){
+                depth[i]--;
+                if(depth[i] == 0)
+                    que.push(i);
+            }
+        }
+        if(result.size() < numCourses)
+            return {};
+        return result;
+    }
+};
+int main() {
+    Solution s;
+    int numCourses = 4;
+    vector<vector<int>> prerequisites = {{1,0},{2,0},{3,1},{3,2}};
+    auto result = s.findOrder(numCourses,prerequisites);
+    for(auto i : result){
+        cout << i << " ";
+    }
+    return 0;
+}
