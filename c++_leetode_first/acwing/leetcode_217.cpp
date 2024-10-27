edefb48fc176a252f8fe1a5cf7a9759f75327edf
XiaoZhijiayou <lzh17737135155@163.com>
2024-10-27 21:44:30 +0800

    力扣217,218

---
+++ c++_leetode_first/acwing/leetcode_217.cpp
@@ -0,0 +1,33 @@
+//
+// Created by 11818 on 2024/10/27.
+//
+#include <unordered_set>
+#include <iostream>
+using namespace  std;
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int>  st;
+        for(auto c : nums){
+            if(st.count(c))    return true;
+            st.insert(c);
+        }
+        return false;
+    }
+};
+int main(){
+    Solution s;
+    vector<int> nums = {1,2,3,1};
+    cout<<s.containsDuplicate(nums)<<endl;
+    return 0;
+}
+//unordered_set 是 C++ 标准库中的容器，和 set 类似，但底层是基于哈希表（hash table）实现的\
+//性质
+//1.元素无序性：unordered_set 中的元素是无序的，插入的顺序与存储顺序不一定一致。
+//2.唯一性：unordered_set 中的元素必须是唯一的，即不能有重复元素。
+//3.高效查找：基于哈希表的实现，使得 unordered_set 查找、插入和删除的时间复杂度平均为𝑂(1)O(1)。
+//4.无序性但恒定性：虽然元素无序，但同一组元素在不同运行中，unordered_set 的顺序可能是恒定的（但在不同编译器或编译器版本下可能不同）。
+//操作
+//1.插入元素：uset.insert(6);
+//2.查找元素：uset.find(3)
+//3.删除元素 uset.erase(4);
---
+++ c++_leetode_first/acwing/leetcode_218.cpp
@@ -0,0 +1,47 @@
+//
+// Created by 11818 on 2024/10/27.
+//
+//线性扫描法
+#include <iostream>
+#include <set>
+#include <utility>
+#include <vector>
+using namespace  std;
+class Solution{
+public:
+    vector<vector<int>> getSkyline(vector<vector<int>>& buildings) {
+        vector<vector<int>> res;
+        multiset<pair<int,int>> events;
+        multiset<int> height;
+        height.insert(0);//为何要先插入一个0？？
+        int n = buildings.size();
+        for(int i = 0; i < n ; i++){
+            events.insert(make_pair(buildings[i][0],-buildings[i][2]));
+            events.insert(make_pair(buildings[i][1],buildings[i][2]));
+        }//左端点的高度是负数，右端点的高度是正数
+        for(auto p : events){
+            if(p.second < 0){
+                //对应上是左端点的话
+                if(-p.second > *height.rbegin())
+                    res.push_back({p.first,-p.second});//保留下最大的那个
+                height.insert(-p.second);
+            }else{
+                //如果为右端点
+                //就是把前面那个端点的高度舍去了
+                height.erase(height.find(p.second));
+                if(p.second > *height.rbegin())
+                    res.push_back({p.first,*height.rbegin()});
+            }
+        }
+        return res;
+    }
+};
+int main(){
+    Solution s;
+    vector<vector<int>> buildings = {{0,2,3},{2,5,3}};
+    vector<vector<int>> res = s.getSkyline(buildings);
+    for(auto a : res){
+        cout << "x坐标：  " << a[0] << "y坐标：   " <<a[1] << endl;
+    }
+    return  0;
+}
